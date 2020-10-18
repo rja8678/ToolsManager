788e36174ab22005d6979c02e7c6e4f9bd92413e
rja8678 <rja8678@rit.edu>
2020-10-18 17:56:12 -0400

    Fixed commandline arguments

--- src/cs/rit/edu/Main.java
+++ src/cs/rit/edu/Main.java
@@ -2,7 +2,12 @@ package cs.rit.edu;
 
 public class Main {
 	public static void main(String [] args) {
-		
-		new DBConn();
+		if(args.length > 1) {
+			String username = args[0];
+			String password = args[1];
+			new DBConn(username, password);
+		}else {
+			System.out.println("Please give the database username and password as commandline arguments");
+		}
 	}
 }
