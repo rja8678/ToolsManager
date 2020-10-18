46cd78e929667833071c5f247c9f2ddafd5f82b4
Zach <zacheas@outlook.com>
2020-10-18 18:10:44 -0400

    Initial commit with db connection and data query.

--- src/cs/rit/edu/DBConn.java
+++ src/cs/rit/edu/DBConn.java
@@ -2,6 +2,9 @@ package cs.rit.edu;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class DBConn {
     Connection c = null ;
     public DBConn(String user, String pass) {
@@ -11,6 +14,7 @@ public class DBConn {
                 c = DriverManager
                         .getConnection("jdbc:postgresql://reddwarf.cs.rit.edu:5432/p320_31",
                                 user, pass);
+                c.setAutoCommit(false);
             } catch (Exception e) {
                 e.printStackTrace();
                 System.err.println(e.getClass().getName() + ": " + e.getMessage());
@@ -24,5 +28,33 @@ public class DBConn {
     }
     public boolean connected() {
         return c != null;
+    }
+
+    public String getTestVal() {
+        Statement stmt = null;
+        if(!connected()) {
+            System.out.println("System not connected.");
+            return null;
+        }
+        try {
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM test;" );
+            while ( rs.next() ) {
+                int id = rs.getInt("id");
+                String val = rs.getString("val");
+
+                System.out.println( "ID = " + id );
+                System.out.println( "Val = " + val );
+
+                System.out.println();
+            }
+            rs.close();
+            stmt.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Operation done successfully");
+        return null;
     }
 }
--- src/cs/rit/edu/Main.java
+++ src/cs/rit/edu/Main.java
@@ -2,7 +2,14 @@ package cs.rit.edu;
 
 public class Main {
 	public static void main(String [] args) {
-		
-		new DBConn();
+		if(args.length > 1) {
+			String username = args[0];
+			String password = args[1];
+
+			DBConn conn = new DBConn(username, password);
+			conn.getTestVal();
+		}else {
+			System.out.println("Please give the database username and password as commandline arguments");
+		}
 	}
 }
