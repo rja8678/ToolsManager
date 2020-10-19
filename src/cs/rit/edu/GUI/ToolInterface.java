0947eec010f5adc98dadf3056ce906cc2983ffc6
rja8678 <rja8678@rit.edu>
2020-10-19 18:58:32 -0400

    Created GUI branch

---
+++ src/cs/rit/edu/GUI/ToolInterface.java
@@ -0,0 +1,38 @@
+package cs.rit.edu.GUI;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ToolInterface extends Application{
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Button btn = new Button();
+        btn.setText("Login");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        stage.setTitle("Tool Interface");
+        stage.setScene(scene);
+        stage.show();
+    }
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
