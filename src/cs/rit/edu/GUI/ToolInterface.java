4967bc8cc87299ad3bbd7b3bb81a9d0a7919350a
rja8678 <rja8678@rit.edu>
2020-10-20 18:44:40 -0400

    Added some logic for creating a new tool

--- src/cs/rit/edu/GUI/ToolInterface.java
+++ src/cs/rit/edu/GUI/ToolInterface.java
@@ -1,5 +1,8 @@
 package cs.rit.edu.GUI;
 
+import java.util.List;
+
+import ObjectClasses.Tool;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -48,6 +51,18 @@ public class ToolInterface extends Application{
         Label toolPurchaseLabel = new Label("Date Purchased: ");
         DatePicker toolPurchaseDate = new DatePicker();
         
+        Button createTool = new Button();
+        createTool.setText("Create Tool");
+        createTool.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Create tool");
+                //TODO Create new tool here and update database
+            }
+        });
+ 
+        
         HBox toolPurchase = new HBox();
         toolPurchase.getChildren().add(toolPurchaseLabel);
         toolPurchase.getChildren().add(toolPurchaseDate);
@@ -70,8 +85,23 @@ public class ToolInterface extends Application{
     }
 	
 	//TODO Run this once user is logged in or the collection for the user has changed
-	public void refreshToolCollection() {
-		
+	/**
+	 * Displays a list of tools for the user representing their collection
+	 * @param tools List of tools to display
+	 */
+	public void refreshToolCollection(List<Tool> tools) {
+		for (Tool tool: tools) {
+			Label toolName = new Label(tool.getToolName());
+			Label purchaseDate = new Label(tool.getPurchaseDate().toString());
+			Label lendable = new Label((tool.isLendable() ? "True": "False"));
+			
+			HBox toolEntry = new HBox();
+			toolEntry.getChildren().add(toolName);
+			toolEntry.getChildren().add(purchaseDate);
+			toolEntry.getChildren().add(lendable);
+			
+			collectionList.getChildren().add(toolEntry);
+		}
 	}
 	
 	public static void main(String[] args) {
