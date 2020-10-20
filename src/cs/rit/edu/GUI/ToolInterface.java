59f67b296253ee4b3213ae6234bdb3905485304f
Mackenzie <mr9376@rit.edu>
2020-10-20 18:53:16 -0400

    Updated GUI to be more user friendly

--- src/cs/rit/edu/GUI/ToolInterface.java
+++ src/cs/rit/edu/GUI/ToolInterface.java
@@ -1,13 +1,12 @@
 package cs.rit.edu.GUI;
 
+import cs.rit.edu.DBConn;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -19,20 +18,47 @@ public class ToolInterface extends Application{
 	@Override
 	public void start(Stage stage) throws Exception {
 		TextField username = new TextField();
-		Button btn = new Button();
-        btn.setText("Login");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+		username.setPromptText("Username");
+		PasswordField password = new PasswordField();
+		password.setPromptText("Password");
+		Button loginBtn = new Button();
+        loginBtn.setText("Login");
+        loginBtn.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
             public void handle(ActionEvent event) {
+                String usernameInput = username.getText();
+                String passwordInput = password.getText();
+
+                if(usernameInput.isBlank() || passwordInput.isBlank()){
+                    System.out.println("Please enter valid credentials!");
+                }else{
+                    DBConn newConnection = new DBConn(usernameInput, passwordInput);
+                    if(newConnection.connected()){
+                        loginBtn.setText("Login Successful!");
+                        loginBtn.setDisable(true);
+
+                    }
+                }
+
                 System.out.println(username.getText());
-                //TODO Log in user
+
             }
         });
+
         //Login at top of the screen
-        HBox hb = new HBox();
-        hb.getChildren().add(username);
-        hb.getChildren().add(btn);
+        VBox usernameAndPassword = new VBox();
+        Label usernameLab = new Label("Username: ");
+        Label passwordLab = new Label("Password: ");
+        usernameAndPassword.getChildren().add(usernameLab);
+        usernameAndPassword.getChildren().add(username);
+        usernameAndPassword.getChildren().add(passwordLab);
+        usernameAndPassword.getChildren().add(password);
+
+        HBox login = new HBox();
+        login.setAlignment(Pos.CENTER_LEFT);
+        login.getChildren().add(usernameAndPassword);
+        login.getChildren().add(loginBtn);
 
         //Collection in the center
         collectionList = new VBox();
@@ -57,7 +83,7 @@ public class ToolInterface extends Application{
         
         
         BorderPane pane = new BorderPane();
-        pane.setTop(hb);
+        pane.setTop(login);
         pane.setCenter(collectionList);
         pane.setLeft(toolCreation);
         
