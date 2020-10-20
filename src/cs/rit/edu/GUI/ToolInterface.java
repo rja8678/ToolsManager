c59cabe3d6a0edd4f0c92eb3a3bb25ea0ad53f4d
rja8678 <rja8678@rit.edu>
2020-10-20 17:53:23 -0400

    Added some of the tool add GUI

--- src/cs/rit/edu/GUI/ToolInterface.java
+++ src/cs/rit/edu/GUI/ToolInterface.java
@@ -5,6 +5,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -16,31 +18,48 @@ public class ToolInterface extends Application{
 	
 	@Override
 	public void start(Stage stage) throws Exception {
-		TextField textField = new TextField ();
+		TextField username = new TextField();
 		Button btn = new Button();
         btn.setText("Login");
         btn.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
             public void handle(ActionEvent event) {
-                System.out.println(textField.getText());
+                System.out.println(username.getText());
                 //TODO Log in user
             }
         });
         //Login at top of the screen
         HBox hb = new HBox();
-        hb.getChildren().add(textField);
+        hb.getChildren().add(username);
         hb.getChildren().add(btn);
 
         //Collection in the center
         collectionList = new VBox();
         
         VBox toolCreation = new VBox();
-//        toolCreation
+        Label toolNameLabel = new Label("Tool Name: ");
+        TextField toolNameEntry = new TextField();
+        
+        HBox toolName = new HBox();
+        toolName.getChildren().add(toolNameLabel);
+        toolName.getChildren().add(toolNameEntry);
+        
+        Label toolPurchaseLabel = new Label("Date Purchased: ");
+        DatePicker toolPurchaseDate = new DatePicker();
+        
+        HBox toolPurchase = new HBox();
+        toolPurchase.getChildren().add(toolPurchaseLabel);
+        toolPurchase.getChildren().add(toolPurchaseDate);
+        
+        toolCreation.getChildren().add(toolName);
+        toolCreation.getChildren().add(toolPurchase);
+        
         
         BorderPane pane = new BorderPane();
         pane.setTop(hb);
         pane.setCenter(collectionList);
+        pane.setLeft(toolCreation);
         
 
         Scene scene = new Scene(pane, 300, 250);
