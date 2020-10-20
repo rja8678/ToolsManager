a9a8d6e72f2456486fea72c293fef9d3007e5a7b
rja8678 <rja8678@rit.edu>
2020-10-20 17:32:24 -0400

    Stubbed a few more GUI elements

--- src/cs/rit/edu/GUI/ToolInterface.java
+++ src/cs/rit/edu/GUI/ToolInterface.java
@@ -6,11 +6,13 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ToolInterface extends Application{
-
+	VBox collectionList;
 	
 	@Override
 	public void start(Stage stage) throws Exception {
@@ -22,18 +24,36 @@ public class ToolInterface extends Application{
             @Override
             public void handle(ActionEvent event) {
                 System.out.println(textField.getText());
+                //TODO Log in user
             }
         });
+        //Login at top of the screen
         HBox hb = new HBox();
         hb.getChildren().add(textField);
         hb.getChildren().add(btn);
 
-        Scene scene = new Scene(hb, 300, 250);
+        //Collection in the center
+        collectionList = new VBox();
+        
+        VBox toolCreation = new VBox();
+//        toolCreation
+        
+        BorderPane pane = new BorderPane();
+        pane.setTop(hb);
+        pane.setCenter(collectionList);
+        
+
+        Scene scene = new Scene(pane, 300, 250);
 
         stage.setTitle("Tool Interface");
         stage.setScene(scene);
         stage.show();
     }
+	
+	//TODO Run this once user is logged in or the collection for the user has changed
+	public void refreshToolCollection() {
+		
+	}
 	
 	public static void main(String[] args) {
 		launch(args);
