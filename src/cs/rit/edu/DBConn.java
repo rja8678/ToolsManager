7f0dd855dbf3176683637e74406fc968cabda06f
Zach <zacheas@outlook.com>
2020-10-20 21:57:28 -0400

    added some stuff to git ignore, hopefully it works

--- src/cs/rit/edu/DBConn.java
+++ src/cs/rit/edu/DBConn.java
@@ -2,8 +2,6 @@ package cs.rit.edu;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class DBConn {
     Connection c = null ;
