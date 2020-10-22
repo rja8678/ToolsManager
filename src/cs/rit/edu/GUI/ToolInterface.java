97a35decb6d688244a6a73a9e097146bf27d92ba
rja8678 <rja8678@rit.edu>
2020-10-22 14:59:35 -0400

    Added more comments

--- src/cs/rit/edu/GUI/ToolInterface.java
+++ src/cs/rit/edu/GUI/ToolInterface.java
@@ -37,6 +37,7 @@ public class ToolInterface extends Application{
     
     @Override
     public void stop() {
+    	//Closing the connection to the database formally
     	conn.closeConn();
     }
     
@@ -47,6 +48,7 @@ public class ToolInterface extends Application{
 
 		Button loginBtn = new Button();
         loginBtn.setText("Login");
+        //Called when the user tries to login
         loginBtn.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
@@ -56,7 +58,8 @@ public class ToolInterface extends Application{
                 System.out.println(Integer.parseInt(usernameInput));
 
                 appUser = new User(Integer.parseInt(usernameInput), conn);
-
+                
+                //Call setup that requires the appUser here
                 refreshToolCollection(appUser.getToolCollection());
                 refreshToolsOwned(appUser.getOwnedTools());
                 lendingPaneInit();
@@ -74,22 +77,16 @@ public class ToolInterface extends Application{
         login.getChildren().add(usernameGroup);
         login.getChildren().add(loginBtn);
 
-        //Collection in the center
-        HBox toolLists = new HBox();
         
-        collectionList = new HBox();
-        ownedList = new HBox();
+        //Collection in the center, holds the collection of tools and owned tools
+        HBox toolLists = createCollections();
         
-        createCollections();
-        
-        toolLists.getChildren().add(collectionList);
-        toolLists.getChildren().add(new Separator());
-        toolLists.getChildren().add(ownedList);
-        
+        //Creates the tool creation menu that allows a user to make a new tool
         VBox toolCreation = toolCreationMenu();
         
         lendPane = new VBox();
         
+        //Main pane that holds all the sub panes
         BorderPane pane = new BorderPane();
         pane.setTop(login);
         pane.setCenter(toolLists);
@@ -103,9 +100,18 @@ public class ToolInterface extends Application{
         stage.show();
     }
 	
-	public void createCollections() {
-        
+	/**
+	 * Creates the "tables" for displaying both the tool collection and owned tools
+	 * Called once on initialization
+	 * @return The HBox holding those tables to be added to the BorderPane
+	 */
+	public HBox createCollections() {
 		//Creating columns for the collection
+		HBox toolLists = new HBox();
+        
+        collectionList = new HBox();
+        ownedList = new HBox();
+		
         collectionToolName = new VBox();
         collectionPurchaseDate = new VBox();
         
@@ -124,6 +130,12 @@ public class ToolInterface extends Application{
         ownedList.getChildren().add(new Separator());
         ownedList.getChildren().add(ownedLendable);
         
+
+        toolLists.getChildren().add(collectionList);
+        toolLists.getChildren().add(new Separator());
+        toolLists.getChildren().add(ownedList);
+        
+        return toolLists;
 	}
 	
 	/**
@@ -146,6 +158,10 @@ public class ToolInterface extends Application{
 		}
 	}
 	
+	/**
+	 * Function to set up the pane allowing the user to lend a tool to someone
+	 * Created once a user logs in
+	 */
 	public void lendingPaneInit() {
 		lendPane.getChildren().clear();
 		
@@ -162,17 +178,26 @@ public class ToolInterface extends Application{
         
         ArrayList<String> usersNames = new ArrayList<>();
         for (Integer userID: allUsers.keySet()) {
+        	//TODO Remove the appUsers name from the list
         	usersNames.add(allUsers.get(userID)+" "+userID);
         }
         
         ComboBox<String> toolsDrop = new ComboBox<String>(FXCollections.observableArrayList(toolNames));
         ComboBox<String> usersDrop = new ComboBox<String>(FXCollections.observableArrayList(usersNames));
         
+        //TODO Find what values are selected on the ComboBoxes then actually send the request to the database
+        
         lendPane.getChildren().add(toolsDrop);
         lendPane.getChildren().add(usersDrop);
 	}
 	
+	/**
+	 * Function to make the tool creation sidebar, added to the main BorderPane
+	 * Called once on initialization
+	 * @return A VBox containing everything necessary to make a tool
+	 */
 	public VBox toolCreationMenu() {
+		//Making the input fields
 		VBox toolCreation = new VBox();
         Label toolNameLabel = new Label("Tool Name: ");
         TextField toolNameEntry = new TextField();
@@ -184,7 +209,6 @@ public class ToolInterface extends Application{
         Label toolPurchaseLabel = new Label("Date Purchased: ");
         DatePicker toolPurchaseDate = new DatePicker();
 
-        
         HBox toolPurchase = new HBox();
         toolPurchase.getChildren().add(toolPurchaseLabel);
         toolPurchase.getChildren().add(toolPurchaseDate);
@@ -197,7 +221,7 @@ public class ToolInterface extends Application{
         lendableTool.getChildren().add(lendableToolCheck);
         
         
-        //Tool Types Checkboxes
+        //Tool Types Checkboxes to select what types this tool is
         HBox listOfTypes = new HBox();
         VBox toolTypeName = new VBox();
         VBox checkBoxes = new VBox();
@@ -205,9 +229,12 @@ public class ToolInterface extends Application{
         listOfTypes.getChildren().add(toolTypeName);
         listOfTypes.getChildren().add(checkBoxes);
         
+        //Getting the tool types from the database
         HashMap<Integer, String> toolTypes = conn.fetchAllToolTypes();
+        //Storing the checkboxes to figure out which were selected on the button press
         List<CheckBox> selectedToolTypes = new ArrayList<>();
         
+        //Creating the checkboxes with labels
         for(String toolType: toolTypes.values()) {
         	Label toolTypeLabel = new Label(toolType);
         	toolTypeName.getChildren().add(toolTypeLabel);
@@ -219,22 +246,26 @@ public class ToolInterface extends Application{
         
         Button createTool = new Button();
         createTool.setText("Create Tool");
+        //Runs when the user clicks to create a new tool
         createTool.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Create tool");
-                //TODO Change
+                //TODO Needs to take the list of checkboxes and tool types names and convert it to a list of tool type id's selected
                 Iterator<String> toolTypesI = toolTypes.values().iterator();
                 Iterator<CheckBox> selectedToolTypesI = selectedToolTypes.iterator();
                 LinkedList<String> output = new LinkedList<>();
                 while(toolTypesI.hasNext()) {
+                	//Checks if the checkbox was selected
                 	if (selectedToolTypesI.next().isSelected()) {
+                		//If it was find the id associated with the tool type name that was selected
+                		//Tool type name is actually toolTypesI.next(), don't call multiple times otherwise it breaks
                 		output.add(toolTypesI.next());
                 	}
                 }
                 
-                //TODO Create the tool on the database
+                //TODO Create the tool on the database using the data from the fields above
+                //Ex: toolPurchaseDate.getValue()
             }
         });
         
@@ -271,6 +302,10 @@ public class ToolInterface extends Application{
 		}
 	}
 	
+	/**
+	 * Connects to the database and starts the GUI interface
+	 * @param args Takes the username to the database and then the password as arguments with just a space in between them
+	 */
 	public static void main(String[] args) {
 		if(args.length > 1) {
 			String username = args[0];
