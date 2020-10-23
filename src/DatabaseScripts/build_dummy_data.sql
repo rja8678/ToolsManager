9a0f35d22c6c61fc16d859762912347883d273cc
Zach <zacheas@outlook.com>
2020-10-23 18:03:15 -0400

    Added mass-data load

--- src/DatabaseScripts/build_dummy_data.sql
+++ src/DatabaseScripts/build_dummy_data.sql
@@ -3,28 +3,28 @@ INSERT INTO "user" (fname, lname) VALUES ('riley','adams');
 INSERT INTO "user" (fname, lname) VALUES ('andrew','nash');
 INSERT INTO "user" (fname, lname) VALUES ('mackenzie','rhody');
 
-INSERT INTO tool (name, lendable, purchase_date) VALUES ('STIHL MSA 161 T', 1, '2020-10-20');
-INSERT INTO tool (name, lendable, purchase_date) VALUES ('Nikro Dewalt 20V Cordless Drill', 1, '2020-10-20');
-INSERT INTO tool (name, lendable, purchase_date) VALUES ('Bosch PS31-2A 12-Volt Max', 0, '2020-10-20');
+-- INSERT INTO tool (name, lendable, purchase_date) VALUES ('STIHL MSA 161 T', 1, '2020-10-20');
+-- INSERT INTO tool (name, lendable, purchase_date) VALUES ('Nikro Dewalt 20V Cordless Drill', 1, '2020-10-20');
+-- INSERT INTO tool (name, lendable, purchase_date) VALUES ('Bosch PS31-2A 12-Volt Max', 0, '2020-10-20');
 
 INSERT INTO tooltype (type_name) VALUES ('power');
 INSERT INTO tooltype (type_name) VALUES ('saw');
 INSERT INTO tooltype (type_name) VALUES ('drill');
 INSERT INTO tooltype (type_name) VALUES ('hammer');
-
-INSERT INTO tool_tooltype VALUES (1,1);
-INSERT INTO tool_tooltype VALUES (1,2);
-
-INSERT INTO tool_tooltype VALUES (2,1);
-INSERT INTO tool_tooltype VALUES (2,3);
-
-INSERT INTO tool_tooltype VALUES (3,1);
-INSERT INTO tool_tooltype VALUES (3,3);
-
-INSERT INTO collection VALUES (1,1);
-INSERT INTO collection VALUES (1,2);
-INSERT INTO collection VALUES (2,3);
-
-INSERT INTO user_owns_tool VALUES (1,1);
-INSERT INTO user_owns_tool VALUES (1,2);
-INSERT INTO user_owns_tool VALUES (2,3);
+--
+-- INSERT INTO tool_tooltype VALUES (1,1);
+-- INSERT INTO tool_tooltype VALUES (1,2);
+--
+-- INSERT INTO tool_tooltype VALUES (2,1);
+-- INSERT INTO tool_tooltype VALUES (2,3);
+--
+-- INSERT INTO tool_tooltype VALUES (3,1);
+-- INSERT INTO tool_tooltype VALUES (3,3);
+--
+-- INSERT INTO collection VALUES (1,1);
+-- INSERT INTO collection VALUES (1,2);
+-- INSERT INTO collection VALUES (2,3);
+--
+-- INSERT INTO user_owns_tool VALUES (1,1);
+-- INSERT INTO user_owns_tool VALUES (1,2);
+-- INSERT INTO user_owns_tool VALUES (2,3);
