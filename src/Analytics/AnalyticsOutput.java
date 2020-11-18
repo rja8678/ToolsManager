747ad33055c3e710235b56bdebbaf66c63a060c5
asn3371 <asn3371@rit.edu>
2020-11-17 19:23:49 -0500

    First version of analyticcs put together

--- src/Analytics/AnalyticsOutput.java
+++ src/Analytics/AnalyticsOutput.java
@@ -3,6 +3,8 @@ package Analytics;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import DBContollerPackage.DBAnalytics;
 import cs.rit.edu.DBConn;
@@ -10,15 +12,38 @@ import cs.rit.edu.DBConn;
 public class AnalyticsOutput {
 	public void generateOutput(DBConn conn) {
 		DBAnalytics analytics = new DBAnalytics(conn);
+		//final 2d array that stores everything
+		List<List<String>> data = new ArrayList<>(); //TODO;
+
+		//first, need to collect all the data into one list of lists.
+
+		List<List<String>> mostLentTypesData = analytics.mostLentType();
+
+		data.addAll(mostLentTypesData);
+
+
+
+
+		StringBuilder result = new StringBuilder();
+
+
+		//builds final "result" string to be printed to file
+		for(int i = 0; i < data.size(); i++){
+			for(int j = 0; j < data.get(0).size(); j++){
+				result.append(data.get(i).get(j));
+				result.append(", ");
+			}
+			result.append("\n");
+		}
 		
 		
 		try {
 			File file = new File("output.csv");
 			
 			FileWriter out = new FileWriter(file);
-			
-
-			analytics.mostLentType();
+			out.write(result.toString());
+			out.flush();
+			out.close();
 			
 		} catch (IOException e) {
 			System.out.println("Unable to open file.");
